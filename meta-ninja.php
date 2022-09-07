80556f6ce150cd82c98f6b09e816b50393f316fd
Bozabit - Daniel Alvarez Echenagucia <alvareze.daniel@gmail.com>
2022-09-07 10:25:53 -0500

    testting 0.1.1

--- meta-ninja.php
+++ meta-ninja.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Meta Ninja
  * Plugin URI:        https://luckyseed.io/wp/plugins/meta-ninja
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Bozabit
  * Author URI:        https://luckyseed.io/people/bozabit
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('META_NINJA_VERSION', '0.1.0');
+define('META_NINJA_VERSION', '0.1.1');
 
 /**
  * The code that runs during plugin activation.
